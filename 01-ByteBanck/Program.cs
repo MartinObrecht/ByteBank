6a2c4384cbd6917d2c01a1b92fc5e2a0b1807c18
Martin Obrecht <martin.obrecht@yahoo.com.br>
2020-12-11 12:35:16 -0300

    Adicionar arquivos de projeto.

---
+++ 01-ByteBanck/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _01_ByteBanck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            primeiraContaCorrente.saldo = 200;
+
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            primeiraContaCorrente.saldo += 100;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.saldo = 50;
+
+            Console.WriteLine("A primeira conta tem " + primeiraContaCorrente.saldo);
+            Console.WriteLine("A segunda conta tem " + segundaContaCorrente.saldo);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,58 @@
+﻿
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+        //Por convenção propriedades começam com letra maiuscula e atributos privados iniciam com underscore "_"     
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }           
+        public bool Sacar(double valor)
+        {
+            if (_saldo <= valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        //Usamos void quando o método/função não retorna nenhum valor.
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
